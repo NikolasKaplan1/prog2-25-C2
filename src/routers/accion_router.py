f02a6c2a651853242660297591b520cf587d2f5c
ppf30 <ppf30@alu.ua.es>
2025-04-10 21:31:26 +0200

    documentación del código

--- src/routers/accion_router.py
+++ src/routers/accion_router.py
@@ -1,18 +1,64 @@
+
+
+"""
+Módulo de rutas para la gestión de acciones en la API Flask
+
+Este módulo define endpoints RESTful para hacer operaciones sobre
+acciones ccomo obtener la lista completa de acciones, consultar una acción por ID 
+y registrar una nueva acción
+
+Utiliza SQLModel para la interacción con la base de datos, y Flask Blueprint para 
+modularizar las rutas
+
+Dependencias
+------------
+- Flask
+- SQLModel
+- models (AccionDB)
+- main (engine de conexión a base de datos)
+"""
+
 from flask import Blueprint, request, jsonify, abort
 from sqlmodel import Session, select
-from models import AccionDB  # Importar el modelo AccionDB
-from main import engine  # Importar el engine desde main.py
+from models import AccionDB  
+from main import engine  
 
 accion_bp = Blueprint("accion", __name__)
 
 @accion_bp.route("/acciones", methods=["GET"])
 def get_acciones():
+    """
+    Obtener todas las acciones registradas en la base de datos
+
+    Returns
+    -------
+    Response:
+        Una lista de objetos JSON que representan las acciones
+    """
     session = Session(engine)
     acciones = session.exec(select(AccionDB)).all()
     return jsonify([accion.model_dump() for accion in acciones])
 
 @accion_bp.route("/acciones/<int:accion_id>", methods=["GET"])
 def get_accion_id(accion_id):
+    """
+    Obtener una acción específica por su identificador único
+
+    Parameters
+    ----------
+    accion_id : int
+        ID de la acción a buscar
+
+    Returns
+    -------
+    Response:
+        Objeto JSON que representa la acción solicitada
+
+    Raises
+    ------
+    404 Not Found:
+        Cuando no se encuentra una acción con el ID proporcionado
+    """
     session = Session(engine)
     accion = session.get(AccionDB, accion_id)
     if accion is None:
@@ -21,11 +67,27 @@ def get_accion_id(accion_id):
 
 @accion_bp.route("/acciones", methods=["POST"])
 def post_nueva_accion():
+    """
+    Crear una nueva acción y guardarla en la base de datos
+
+    El cuerpo de la solicitud debe ser un JSON que contenga los campos:
+    "simbolo", "nombre" y "precio_actual"
+
+    Returns
+    -------
+    Tuple[Response, int]:
+        El objeto JSON que representa la acción creada y el código de estado 201
+
+    Raises
+    ------
+    400 Bad Request:
+        Si faltan campos obligatorios en el body de la solicitud
+    """
     data = request.get_json()
-    if not all(key in data for key in ("simbolo", "nombre", "precio")):
+    if not all(key in data for key in ("simbolo", "nombre", "precio_actual")):
         abort(400, description="Faltan campos obligatorios por completar")
     
-    nuevo = AccionDB(simbolo=data["simbolo"], nombre=data["nombre"],precio=data["precio"] )
+    nuevo = AccionDB(simbolo=data["simbolo"], nombre=data["nombre"],precio=data["precio_actual"] )
     
     session = Session(engine)
     session.add(nuevo)
--- src/routers/inversor_router.py
+++ src/routers/inversor_router.py
@@ -1,19 +1,61 @@
+
+"""
+Módulo de rutas para la gestión de inversores en la API Flask.
+
+Proporciona endpoints RESTful para consultar y registrar inversores en la base de 
+datos, utiliza SQLModel para el manejo de datos relacional y Flask Blueprint para 
+la organización modular del código
+
+Dependencias
+------------
+- Flask
+- SQLModel
+- models (InversorDB)
+- main (engine de conexión a la base de datos)
+"""
+
 from flask import Blueprint, request, jsonify, abort
 from sqlmodel import Session, select
-from models import InversorDB  # Importar el modelo AccionDB
-from main import engine  # Importar el engine desde main.py
+from models import InversorDB  
+from main import engine  
 from datetime import datetime
 
 inversor_bp = Blueprint("inversor", __name__)
 
 @inversor_bp.route("/inversores", methods=["GET"])
 def get_inversor():
+    """
+    Obtener todos los inversores registrados en la base de datos
+
+    Returns
+    -------
+    Response:
+        Lista de objetos JSON que representan los inversores
+    """
     session = Session(engine)
     inversores = session.exec(select(InversorDB)).all()
     return jsonify([inversor.model_dump() for inversor in inversores])
 
 @inversor_bp.route("/inversores/<int:inversor_id>", methods=["GET"])
 def get_inversor_id(inversor_id):
+    """
+    Obtener un inversor específico por su identificador
+
+    Parameters
+    ----------
+    inversor_id : int
+        ID del inversor a consultar
+
+    Returns
+    -------
+    Response:
+        Objeto JSON que representa el inversor solicitado
+
+    Raises
+    ------
+    404 Not Found:
+        Si no se encuentra un inversor con el ID indicado
+    """
     session = Session(engine)
     inversor = session.get(InversorDB, inversor_id)
     if inversor is None:
@@ -22,6 +64,22 @@ def get_inversor_id(inversor_id):
 
 @inversor_bp.route("/inversores", methods=["POST"])
 def post_nuevo_inversor():
+    """
+    Registrar un nuevo inversor en la base de datos
+
+    El cuerpo de la solicitud debe contener los siguientes campos:
+    "nombre", "apellidos", "email", "contrasena", "tarjeta_credito" y "capital"
+
+    Returns
+    -------
+    Tuple[Response, int]:
+        Objeto JSON que representa al inversor creado y código de estado 201
+
+    Raises
+    ------
+    400 Bad Request:
+        Si falta alguno de los campos requeridos en la solicitud
+    """
     session = Session(engine)
     data = request.get_json()
     if not all(key in data for key in ("nombre", "apellidos", "email", "contrasena", "tarjeta_credito", "capital")):
